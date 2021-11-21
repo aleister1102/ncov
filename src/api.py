36ada30fa574ddd086a979d29ed65f066bf6aa70
marucube35 <marucube35@gmail.com>
2021-11-21 17:05:04 +0700

    create time for update

--- src/api.py
+++ src/api.py
@@ -2,7 +2,10 @@ import requests
 import json
 import os.path
 import database as db
+import datetime
 from string import Template
+
+
 
 
 # World API
@@ -13,7 +16,46 @@ WORLD_CODE = 'src/countries_code.json'
 # Vietnam API
 
 VIETNAM = 'https://api.apify.com/v2/key-value-stores/EaCBL1JNntjR3EakU/records/LATEST?disableRedirect=true&utf8=1'
-FILE_VIETNAM = 'src/vietnam_specific.json'
+VIETNAM_FILE = 'src/vietnam_specific.json'
+
+#Time
+TIME_FILE = 'src/time.json'
+
+#! Move to database.py
+def getCurrentTime():
+    
+    time = datetime.datetime.now()
+    #Trả về một dictionary
+    return {'year': time.year, 'month': time.month, 'day':time.day, 
+            'hour':time.hour, 'minute':time.minute, 'second':time.second}
+    
+def readLatestTime():
+    
+    f = open(TIME_FILE,'r')
+    time = json.load(f)
+    #Trả về một dictionary cho thời gian gần đây
+    return time[-1]
+
+def isUpdated():
+    
+    latestTime = readLatestTime()
+    currentTime = getCurrentTime()
+    
+    hourDiff = currentTime['hour'] - latestTime['hour']
+    minDiff = currentTime['minute'] - latestTime['minute']
+    
+    if(hourDiff > 1):
+        return False
+    if(hourDiff == 1):
+        if(minDiff >=0):
+            return False
+        else:
+            return True
+    else:
+        return True
+        
+
+    
 
 def fetchCountry(name):
     
@@ -21,14 +63,21 @@ def fetchCountry(name):
     Cập nhật thông tin covid của một quốc gia
     name: tên quốc gia
     """
+    #Nếu đã cập nhật thì return
+    if(isUpdated()):
+        return
+    
+    #Loại bỏ quốc gia bị lỗi
     print('Fetching',name)
     if(name == 'Saint Vincent and Grenadines'):
         return
 
+    #Lấy API
     response = requests.get(Template(WORLD_API).substitute(name = name))
     if(response.status_code != 200):
         return
     
+    #Kiểm tra file của nước bất kỳ có tồn tại chưa
     filePath = Template(WORLD_FILE).substitute(name = name)
     if(os.path.isfile(filePath)):
         print('File is existed')
@@ -47,6 +96,7 @@ def fetchWorld():
     
     for country in worlds:
         fetchCountry(country['country'])
+    f.close()
 
 def fetchVietnam(option):
 
@@ -55,14 +105,10 @@ def fetchVietnam(option):
     option: tên tỉnh thành
     """
     responseVietnam = json.loads(requests.get(VIETNAM).content)['locations']
-    db.updateJSON(FILE_VIETNAM, responseVietnam)
+    db.updateJSON(VIETNAM_FILE, responseVietnam)
 
     # for province in responseVietnam:
     #     if(province['name'] == option):
     #         print(province)
 
-
-
-
-    
-    
+isUpdated()
