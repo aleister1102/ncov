a1d4d9f08863470290dde3a47e43be82a37a0656
marucube35 <marucube35@gmail.com>
2021-11-21 15:38:07 +0700

    clean file

--- src/api.py
+++ src/api.py
@@ -6,22 +6,14 @@ from string import Template
 
 
 # World API
-TWO_DAYS_AGO = 'two days ago'
-WORLD_TWO_DAYS_AGO = 'https://disease.sh/v3/covid-19/countries?twoDaysAgo=true'
-FILE_WORLD2 = 'src/world_two_days_ago.json'
-
-YESTERDAY = 'yesterday'
-WORLD_YESTERDAY = 'https://disease.sh/v3/covid-19/countries?yesterday=true'
-FILE_WORLD1 = 'src/world_yesterday.json'
-
-TODAY = 'today'
-WORLD = 'https://disease.sh/v3/covid-19/countries'
-FILE_WORLD = 'src/world.json'
+WORLD_API = 'https://api.covid19api.com/dayone/country/$name/status/confirmed'
+WORLD_FILE = 'src/countries/$name.json'
+WORLD_CODE = 'src/countries_code.json'
 
 # Vietnam API
 
 VIETNAM = 'https://api.apify.com/v2/key-value-stores/EaCBL1JNntjR3EakU/records/LATEST?disableRedirect=true&utf8=1'
-FILE_VIETNAM = 'src/vietnam.json'
+FILE_VIETNAM = 'src/vietnam/vietnam.json'
 
 # def getCountriesCode():
 
@@ -39,40 +31,35 @@ FILE_VIETNAM = 'src/vietnam.json'
 
 def fetchCountry(name):
     
-    print('fetching',name)
+    print('Fetching',name)
     if(name == 'Saint Vincent and Grenadines'):
         return
-    try:
-        response = requests.get(Template('https://api.covid19api.com/dayone/country/$name/status/confirmed').substitute(name = name))
-        if(response.status_code != 200):
-            return
-        
-        string = json.loads(response.content)
-        db.updateJSON(Template('src/countries/$name.json').substitute(name = name),string)
-    except:
+
+    response = requests.get(Template(WORLD_API).substitute(name = name))
+    if(response.status_code != 200):
         return
+    
+    filePath = Template(WORLD_FILE).substitute(name = name)
+    if(os.path.isfile(filePath)):
+        print('File is existed')
+        return
+    
+    string = json.loads(response.content)
+    db.updateJSON(filePath,string)
 
 def fetchWorld():
-    """
-    Fetch Covid information of world.
-    option: date
-    """
-
-    f = open('src/countries_code.json', 'r')
+    
+    f = open(WORLD_CODE, 'r')
     worlds = json.load(f)
     
     for country in worlds:
         fetchCountry(country['country'])
 
-def getWorld(option):
+
+def getWorld(name):
     
-    f = open('src/countries_code.json', 'r')
-    worlds = json.load(f)
-    i = 0
-    for country in worlds:
-        i+=1
-        print(i)
-        print(country)
+    pass
+ 
 
 
 def fetchVietnam(option):
@@ -80,18 +67,17 @@ def fetchVietnam(option):
     responseVietnam = json.loads(requests.get(VIETNAM).content)['locations']
     db.updateJSON(FILE_VIETNAM, responseVietnam)
 
-    for province in responseVietnam:
-        if(province['name'] == option):
-            print(province)
+    # for province in responseVietnam:
+    #     if(province['name'] == option):
+    #         print(province)
 
 
 def getVietnam():
 
-    file = open(FILE_VIETNAM, 'r')
-    return json.load(file)
+    pass
 
 
-fetchWorld()
+
 
     
     
